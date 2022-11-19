718609259cd44abb8a131e9c8098aeb76d3c46b9
hoowo <redmipro2022@hoowoo.com>
2022-11-20 02:36:17 +0800

    修改跨域设置

--- onsite_v2/onsite_v2/settings.py
+++ onsite_v2/onsite_v2/settings.py
@@ -175,15 +175,8 @@ JWT_AUTH = {
 }
 
 # 跨域请求
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:8080',
-    'http://localhost:8080',
-    'http://10.231.3.21:8080',
-    'http://10.231.3.21:80',
-    'http://127.0.0.1:80',
-    'http://127.0.0.1:80',
-    'http://localhost:80',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = ['*']
 
 CORS_ALLOW_CREDENTIALS = True
